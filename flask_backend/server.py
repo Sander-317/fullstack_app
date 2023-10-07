d4efd0e35e64bfbbdc3f15dfc4a6ada20f479278
Sander-317 <sander.van.meerveld@gmail.com>
2023-10-07 14:10:57 +0200

    made back end talk to front end

--- flask_backend/server.py
+++ flask_backend/server.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/test')
+@app.route('/test', methods=['GET'])
 def test():
+    # return {"test"}
     return {"test": ["test1", "test2", "test3"]}
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8000 ,debug=True)
